7d8dcdbbda69159c5817d5f6fe1bb28c9307a5b6
LAPTOP-B5GQ48GP\dns <katmalceva22@gmail.com>
2021-05-14 01:27:10 +0300

    fix my-profile.module (added RouterModule)

--- src/app/pages/my-profile/my-profile.module.ts
+++ src/app/pages/my-profile/my-profile.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyProfileComponent } from './my-profile.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [MyProfileComponent],
-  imports: [CommonModule],
-  exports: [MyProfileComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [MyProfileComponent, RouterModule],
 })
 export class MyProfileModule {}
